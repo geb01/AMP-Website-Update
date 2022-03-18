a09428a177baf488eb13250be225380bf73373af
Gabriel Azores <gabeo.go10.go@gmail.com>
2022-03-18 15:15:45 +0800

    Final updates

--- about.php
+++ about.php
@@ -64,6 +64,18 @@
 					</div>
 				</div>
 			</section>
+			<section class='transition' id='core-competencies'>
+				<div class='wrapper-core'>
+					<h1 class='feature-display'>Core Competencies</h1>
+					<h1>Core Competencies</h1>
+
+					<div class='feature-core'>
+						<p>Music Production: Providing members opportunities to show their talents and skills through their output within and beyond the organization.</p>
+						<p>Music Appreciation: Creating avenues which aim to foster a sense of appreciation and critical thinking of the universality and impact of music through the initiation of discourse on its different forms in our productions, online platforms, and other related avenues.</p>
+						<p>Music Development: Honing the talents of members through skills training and development in different competencies in relation to music.</p>
+					</div>
+				</div>
+			</section>
 			<section class='transition' id='executive-board'>
 				<div class='wrapper'>
 					<h1 class='dropdown-button'>The Executive Board<div class='arrow'></div></h1>
@@ -71,7 +83,6 @@
 						<?php echo $meta['executive board']; ?>
 					</p>
 				</div>
-				<img src='<?php echo $meta['eb group photo']; ?>'>
 			</section>
 			<section class='feature'>
 				<div class='feature-labels'><?php
@@ -86,7 +97,9 @@
 				</div>
 				<div class='feature-wrapper'><?php
 					$id = 0;
-					foreach ($eb as &$executive) {?>
+					foreach ($eb as &$executive) {
+						$image = $executive['image'];
+						if ($image) $image = "src='$image'"; ?>
 						<div class='feature-display' index='<?php echo $id++; ?>'>
 							<img class='feature-pic' <?php echo $image; ?>>
 							<div class='feature-description'>
@@ -122,7 +135,10 @@
 				</div>
 				<div class='feature-wrapper'><?php
 					$id = 0;
-					foreach ($department as &$dep) { ?>
+					foreach ($department as &$dep) {
+						$image = $dep['image'];
+						if ($image) $image = "src='$image'"; ?>
+
 						<div class='feature-display' index='<?php echo $id++; ?>'>
 							<img class='feature-pic' <?php echo $image; ?>>
 							<div class='feature-description'>
